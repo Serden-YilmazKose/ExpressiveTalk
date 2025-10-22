cb42ae180465566cfb151150c8371a1d1527f389
Ahmedsalah882 <85747841+Ahmedsalah882@users.noreply.github.com>
2025-10-22 18:49:29 +0300

    This script extracting the frames from styled video

--- styletalk/styletalk.py
+++ styletalk/styletalk.py
@@ -19,7 +19,15 @@ def generate_styletalk_video(style_video_path, pose_path, reference_image_path, 
     """
 
     os.system(cmd)
-    return output_path
+    cap = cv2.VideoCapture(out_path)
+    frames = []
+    ok, f = cap.read()
+    while ok
+        frames.append(f)
+        ok, f = cap.read()
+    cap.release()
+    return frames
+    #return output_path
 
 if __name__ == "__main__":
     generate_styletalk_video(style_video_path="./ref_img", pose_path="./", reference_image_path="./styletalk/samples/source_video/image/andrew_clip_1.png", output_path="./", audio_path="Obama_clip1.wav")
