ca2f16a667de495ff38e2d2fd770b9147c2321b1
Jesper Nyman <jnyman21@student.oulu.fi>
2025-10-22 16:18:56 +0200

    added requirements.txt and a sh script to setup styletalk and Deep3DFaceRecon dependencies

---
+++ styletalk/setup_environment.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+echo "Installing system dependencies..."
+apt-get update
+apt-get install -y build-essential cmake libgl1-mesa-glx libglib2.0-0 ffmpeg
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Cloning Deep3DFaceRecon_pytorch"
+git clone https://github.com/sicxu/Deep3DFaceRecon_pytorch.git
+echo "Cloning styletalk"
+git clone https://github.com/FuxiVirtualHuman/styletalk.git
+
+echo "Installing nvdiffrast"
+git clone https://github.com/NVlabs/nvdiffrast temp_1
+cd temp_1
+pip install .
+cd ..
+rm -rf temp_1
+
+echo "Installing ArcFace Torch (InsightFace)..."
+git clone https://github.com/deepinsight/insightface.git temp_2
+cp -r temp_2/recognition/arcface_torch Deep3DFaceRecon_pytorch/models/
+rm -rf temp_2
+
+echo "Downloading model assets..."
+
+# Create dirs if needed
+mkdir -p Deep3DFaceRecon_pytorch/checkpoints
+mkdir -p Deep3DFaceRecon_pytorch/checkpoints/epoch_20
+mkdir -p Deep3DFaceRecon_pytorch/BFM
+mkdir -p Deep3DFaceRecon_pytorch/BFM/temp
+
+pip install gdown
+
+echo "Downloading Deep3DFaceRecon checkpoint..."
+gdown 1BlDBB4dLLrlN3cJhVL4nmrd_g6Jx6uP0 \
+    -O Deep3DFaceRecon_pytorch/checkpoints/epoch_20/epoch_20.pth
+
+echo "Downloading Basel Face Model (BFM09)..."
+gdown 1bw5Xf8C12pWmcMhNEu6PtsYVZkVucEN6 \
+    -O Deep3DFaceRecon_pytorch/BFM/Exp_Pca.bin
+wget --user "jnyman21@student.oulu.fi" --password "xlf6ScXc" https://faces.dmi.unibas.ch/bfm/content/basel_face_model/downloads/restricted/BaselFaceModel.tgz \
+    -O "Deep3DFaceRecon_pytorch/BFM/temp/BaselFaceModel.tgz"
+tar -xvzf Deep3DFaceRecon_pytorch/BFM/temp/BaselFaceModel.tgz -C Deep3DFaceRecon_pytorch/BFM/temp
+cp "Deep3DFaceRecon_pytorch/BFM/temp/PublicMM1/01_MorphableModel.mat" "Deep3DFaceRecon_pytorch/BFM"
+
+
+echo "Getting scripts from PIRender repo..."
+wget "https://raw.githubusercontent.com/RenYurui/PIRender/refs/heads/main/scripts/face_recon_videos.py" -O "./Deep3DFaceRecon_pytorch/face_recon_videos.py"
+wget "https://raw.githubusercontent.com/RenYurui/PIRender/refs/heads/main/scripts/extract_kp_videos.py" -O "./Deep3DFaceRecon_pytorch/extract_kp_videos.py"
+wget "https://raw.githubusercontent.com/RenYurui/PIRender/refs/heads/main/scripts/coeff_detector.py" -O "./Deep3DFaceRecon_pytorch/coeff_detector.py"
+wget "https://raw.githubusercontent.com/RenYurui/PIRender/refs/heads/main/scripts/inference_options.py" -O "./Deep3DFaceRecon_pytorch/options/inference_options.py"
+
+echo "All necessary assets downloaded!"
