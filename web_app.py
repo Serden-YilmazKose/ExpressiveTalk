ce73fd5b768ae5ac83d5a3d609d9f2f8fb21ecc8
Patissema <mathis.patigny@insa-rennes.fr>
2025-10-23 23:00:30 +0300

    new link again

--- web_app.py
+++ web_app.py
@@ -5,7 +5,7 @@ import sys
 import gdown
 
 CHECKPOINT_PATH = "checkpoints/wav2lip_gan.pth"
-URL = f"https://drive.usercontent.google.com/download?id=1_OvqStxNxLc7bXzlaVG5sz695p-FVfYY&export=download&authuser=0"
+URL = f"https://drive.google.com/file/d/16XV9XA_cwVhpo-QhMqpGJMRBlLPVc8gO/view?usp=sharing"
 
 # Ensure checkpoints folder exists
 os.makedirs("checkpoints", exist_ok=True)
